32552ee611aef51e14062657bfc0a1fc49d15fd5
Mark <warnick.mark@me.com>
2013-05-28 21:54:51 -0500

    Initial Commit

---
+++ MWGeoFencing/MWGeoFencing.h
@@ -0,0 +1,13 @@
+//
+//  MWGeoFencing.h
+//  MWGeoFencing
+//
+//  Created by Mark Warnick on 5/28/13.
+//  Copyright (c) 2013 Mark Warnick. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MWGeoFencing : NSObject
+
+@end
---
+++ MWGeoFencing/MWGeoFencing.m
@@ -0,0 +1,13 @@
+//
+//  MWGeoFencing.m
+//  MWGeoFencing
+//
+//  Created by Mark Warnick on 5/28/13.
+//  Copyright (c) 2013 Mark Warnick. All rights reserved.
+//
+
+#import "MWGeoFencing.h"
+
+@implementation MWGeoFencing
+
+@end
